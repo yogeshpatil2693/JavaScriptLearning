58cca211e34e3846fa9245183d001cd262c6f311
yogeshpatil2693 <165195110+yogeshpatil2693@users.noreply.github.com>
2025-01-22 13:10:17 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+// alert(3 + 3) //we are using node js and not browser
+
+// mdn and tc39 - for documentation
+
+let name = "Yogesh"
+
+/*
+1.number --> 2 to the power 53
+2.bigint
+3.String = ""
+4.boolean = true / false
+5.null - standalone value and also datatypes which is empty 
+6.undefined - value is not defined variable is declared but not defined
+7.symbol -> unique
+
+
+1.object
+*/
+
+console.log(typeof "yogesh");
+console.log(typeof null);
+
